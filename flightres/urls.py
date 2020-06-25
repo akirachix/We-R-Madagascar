2219232722d5b2d7059b143e036a93b11c4ea6e6
Manoj Bhatt <mbhatt997@gmail.com>
2020-06-25 16:27:50 +0545

    permission detail view added (#7)

--- flightres/urls.py
+++ flightres/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 # importing views from views..py
-from .views import FlightPermissionList
+from .views import FlightPermissionList, flightPermissionDetail, approvePerm
 
 urlpatterns = [
     path('', FlightPermissionList.as_view(), name="dashboard"),
+    path('flightperm_detail/<int:pk>', flightPermissionDetail, name='flightperm-detail'),
+    path('approve_perm/<int:pk>/<str:action>', approvePerm, name='approve-perm'),
 ]
