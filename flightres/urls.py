4f9915d923736c142ca46766653bc3bbab8102f9
Nishon Tandukar <nishon.tan@gmail.com>
2020-07-07 13:49:59 +0545

    resloved conflict

--- flightres/urls.py
+++ flightres/urls.py
@@ -5,7 +5,6 @@ from .views import FlightPermissionList, approvePerm, flightReqResponseView
 
 urlpatterns = [
     path('', FlightPermissionList.as_view(), name="dashboard"),
-    # path('flightperm_detail/<int:pk>', flightPermissionDetail, name='flightperm-detail'),
     path('approve_perm/<int:pk>/<str:action>', approvePerm, name='approve-perm'),
     path('request_response/<int:pk>', flightReqResponseView, name='request-response'),
 ]
