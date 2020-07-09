999734cfc752d01bdb14ad87ee31831081552d3c
Nishon Tandukar <nishon.tan@gmail.com>
2020-07-09 07:26:11 +0545

    modified premission template

--- flightres/views.py
+++ flightres/views.py
@@ -9,23 +9,26 @@ from .models import FlightPermission, Report
 def homeView(request):
     return render(request, 'flightres/home.html')
 
+
 @login_required
 def dashboardView(request):
     return render(request, 'flightres/dashboard.html')
+
 
 class FlightPermissionList(LoginRequiredMixin, ListView):
     # specify the model for list view
     model = FlightPermission
 
+
 @login_required
-def approvePerm(request,pk, action):
+def approvePerm(request, pk, action):
     selected_perm = get_object_or_404(FlightPermission, uav_uid=pk)
     if action == 'approve':
         selected_perm.status = 'Approved'
     elif action == 'deny':
         selected_perm.status = 'Rejected'
     selected_perm.save()
-    return redirect('/np/dashboard')
+    return redirect('/np/dashboard/permission')
 
 
 def flightReqResponseView(request, pk):
@@ -37,6 +40,7 @@ def flightReqResponseView(request, pk):
     else:
         return render(request, 'flightres/pending_pg.html')
 
+
 @login_required
 def updateComplain(request, pk, action):
     selected_complain = get_object_or_404(Report, uav_uid=pk)
@@ -47,6 +51,7 @@ def updateComplain(request, pk, action):
     selected_complain.save()
     return redirect('/np/dashboard/complain')
 
+
 @login_required
 def submitReply(request, pk):
     selected_complain = get_object_or_404(Report, uav_uid=pk)
@@ -54,6 +59,7 @@ def submitReply(request, pk):
     selected_complain.reply = request.POST.get('reply')
     selected_complain.save()
     return redirect('/np/dashboard/complain')
+
 
 class ComplainListView(LoginRequiredMixin, ListView):
     template_name = 'flightres/complaint_management.html'
