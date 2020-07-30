47edd0d280bc6562305b93510e290201b386f32b
Nishon Tandukar <nishon.tan@gmail.com>
2020-07-30 12:11:40 +0545

    Adds twilio

--- api/twilio.py
+++ api/twilio.py
@@ -3,11 +3,14 @@ import os
 
 
 class Twilio:
-    # ACCOUNT_SID = os.getenv("ACCOUNT_SID")
-    # AUTH_TOKEN = os.getenv("AUTH_TOKEN")
-    # client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    ACCOUNT_SID = os.getenv("ACCOUNT_SID")
+    AUTH_TOKEN = os.getenv("AUTH_TOKEN")
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
     def send_message(self, receiver, message):
-        # message = self.client.messages.create(,,
-
-        print('a')
+        message = self.client.messages.create(  # message = self.client.messages.create(,,
+            from_=receiver,
+            body=message,
+            to=receiver
+        )
+        print(message.sid)
