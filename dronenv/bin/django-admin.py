33ba23c693f1554702229d99f49fe3fa443950cb
imanoj997 <mbhatt997@gmail.com>
2020-08-14 15:46:23 +0545

    permisson deny popup

--- dronenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/siddharth/Workstation/Projects/Django Projects/drone-registration/dronenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
