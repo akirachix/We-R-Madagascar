3da027e09f4c798a010aba8d2b9b4cf6017886ce
nishontan <nishon.tan@gmail.com>
2020-09-24 16:08:44 +0545

    updated message

--- api/twilio.py
+++ api/twilio.py
@@ -9,7 +9,7 @@ class Twilio:
     client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
     def send_report_reply(self,receiver,reply,category,complain):
-        message = """'{}'\nA reply for the report, you had reported on the category '{}' stating '{}'
+        message = """'{}' A reply for the report, you had reported on the category '{}' stating '{}'
         """.format(reply,category,complain)
 
         self.send_message(receiver,message)
