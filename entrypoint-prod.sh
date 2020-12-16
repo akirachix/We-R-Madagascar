b4ff66ab4fe7daefffc7a45c5350d37817008f59
mahesh-naxa <maheshwor.naxa@gmail.com>
2020-12-16 11:19:33 +0545

    added several ignore files

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python3 manage.py collectstatic --noinput
-python manage.py makemigrations --merge --noinput
-python manage.py makemigrations --noinput
+#python manage.py makemigrations --merge --noinput
+#python manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 uwsgi --ini uwsgi.ini
