89cd63d62baef25000aa2cdea884d2554e704aca
wendyorengo <wenndyorengo@gmail.com>
2022-01-25 19:58:45 +0300

    update gdal

--- ohio/settings.py
+++ ohio/settings.py
@@ -226,10 +226,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'root') 
 
-GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('/usr/local/lib/libgdal.dylib')
-GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('/usr/local/Cellar/gdal/3.4.1/lib/libgdal.dylib')
+# GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('/usr/local/lib/libgdal.dylib')
+# GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('/usr/local/Cellar/gdal/3.4.1/lib/libgdal.dylib')
 
-GEOS_LIBRARY_PATH = os.getenv('/usr/local/lib/libgdal.dylib')
+# GEOS_LIBRARY_PATH = '/usr/local/lib/libgdal.dylib'
+GDAL_LIBRARY_PATH  = '/usr/local/Cellar/gdal/3.4.1_1 '
 
 
 import dj_database_url 
