da900947baa6247349542cdf26c45e13e25b572f
wendyorengo <wenndyorengo@gmail.com>
2022-02-13 15:02:26 +0300

    add view for uploading csv

--- clinic/views.py
+++ clinic/views.py
@@ -3,35 +3,45 @@ from django.shortcuts import render
 from django.contrib import messages
 from .models import Clinic
 
+
 def clinic_upload(request):
-    # declaring the template
-    template = "upload_clinics.html"
-    data = Clinic.objects.all()
-
-    # prompt is a context variable that can have different values      depending on their context
-    prompt = {
-        'order': 'Order of the CSV should be name, email, address,    phone, profile',
-        'Clinic': data    
-              }
-
-    # GET request returns the value of the data with the specified key.
-    if request.method == "GET":
-        return render(request, template, prompt)
-    csv_file = request.FILES['file']
-    # let's check if it is a csv file
-    if not csv_file.name.endswith('.csv'):
-        messages.error(request, 'THIS IS NOT A CSV FILE')
-    data_set = csv_file.read().decode('UTF-8')
-
-    # setup a stream which is when we loop through each line we are able to handle a data in a stream
-    io_string = io.StringIO(data_set)
-    next(io_string)
-    for column in csv.reader(io_string, delimiter=',', quotechar="|"):
-        _, created = Clinic.objects.update_or_create(
-            name=column[0],
-            email=column[1],
-            address=column[2],
-            profile=column[4]
-        )
-    context = {}
-    return render(request, template, context)
+	data = {}
+	if "GET" == request.method:
+		return render(request, "clinic/templates/upload_clinics.html", data)
+  # if not GET, then proceed
+	try:
+		csv_file = request.FILES["csv_file"]
+		if not csv_file.name.endswith('.csv'):
+			messages.error(request,'File is not CSV type')
+			return HttpResponseRedirect(reverse("clinic:upload-csv"))
+    #if file is too large, return
+		if csv_file.multiple_chunks():
+			messages.error(request,"Uploaded file is too big (%.2f MB)." % (csv_file.size/(1000*1000),))
+			return HttpResponseRedirect(reverse("clinc:upload_csv"))
+ 
+		file_data = csv_file.read().decode("utf-8")		
+ 
+		lines = file_data.split("\n")
+		#loop over the lines and save them in db. If error , store as string and then display
+		for line in lines:						
+			fields = line.split(",")
+			data_dict = {}
+			data_dict["name"] = fields[0]
+			data_dict["email"] = fields[1]
+			data_dict["address"] = fields[2]
+			data_dict["profile"] = fields[3]
+			try:
+				form = EventsForm(data_dict)
+				if form.is_valid():
+					form.save()					
+				else:
+					logging.getLogger("error_logger").error(form.errors.as_json())												
+			except Exception as e:
+				logging.getLogger("error_logger").error(repr(e))					
+				pass
+ 
+	except Exception as e:
+		logging.getLogger("error_logger").error("Unable to upload file. "+repr(e))
+		messages.error(request,"Unable to upload file. "+repr(e))
+ 
+	return HttpResponseRedirect(reverse("clinic:upload-csv"))
