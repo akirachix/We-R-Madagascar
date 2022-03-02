cb215e5566c1a198d33ffedf298e59c4f0cfa3e3
wendyorengo <wenndyorengo@gmail.com>
2022-03-02 10:04:30 +0300

    create upload clinic document

--- clinic/urls.py
+++ clinic/urls.py
@@ -1,10 +1,12 @@
+
+from django.contrib import admin
 from django.urls import path
-from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
+from clinic.views import clinic_upload
 
-urlpatterns=[
-    path('register-clinic/', ClinicCreateView.as_view(), name='register_clinic'),
-    path('view-clinics/', ClinicViewDetails.as_view(), name='view_clinic'),
-    path('edit-clinic/<int:pk>/', ClinicUpdateView.as_view(), name='edit_clinic'),
-    # path('search/', views.search_clinic, name='search'),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('upload-csv/', clinic_upload, name='clinic_upload'),
 ]
