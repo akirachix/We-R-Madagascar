164f9a4d4d86539a8e41d8bdcd5ac3ec1672b6de
Wuanitawanjiku <ndichujoyce8@gmail.com>
2022-03-18 23:38:19 +0300

    shipments changes

--- shipments/models.py
+++ shipments/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.db.models.signals import post_save
+import datetime as dt
+
+
 
 STATUS_CHOICES = (
- ('pending','pending'),
- ('processed', 'processed'),
- ('dispatched','dispatched'),
+ ('Pending','Pending'),
+ ('Processed', 'Processed'),
+ ('Dispatched','Dispatched'),
+ ('Delayed','Delayed'),
+ ('Completed','Completed'),
+
 )
 # Create your models here.
 class Schedule(models.Model):
@@ -12,22 +19,17 @@ class Schedule(models.Model):
     medication = models.CharField(max_length=20)
     units = models.CharField(max_length=5)
     delivery_date = models.DateField()
-    take_of_time= models.DateField()
-    delivery_time = models.DateField()
+    take_of_time= models.TimeField(default=dt.time(00, 00))
+    delivery_time = models.TimeField(default=dt.time(00, 00))
     status = models.CharField(max_length=30, choices=STATUS_CHOICES,default='dispatched')
     destination = models.CharField(max_length=20)
+    delay_reasons= models.CharField(max_length=20,default='Weather')
+
+
+
 
     def __str__(self):
-        return self.shipment_id
+        return self.status
 
-class Delayed(models.Model):
-    shipment_id = models.CharField(max_length=7)
-    clinic_name = models.CharField(max_length=20)
-    medication = models.CharField(max_length=20)
-    units = models.CharField(max_length=5)
-    delivery_date = models.DateField()
-    take_of_time= models.DateField()
-    delivery_time = models.DateField()
-    status = models.CharField(max_length=30, choices=STATUS_CHOICES,default='dispatched')
-    destination = models.CharField(max_length=20)
-    delay_reasons = models.CharField(max_length=50)
+
+
--- shipments/views.py
+++ shipments/views.py
@@ -3,9 +3,9 @@ from .models import *
 from django.http.response import HttpResponseRedirect
 from django.urls.base import reverse_lazy 
 from django.views.generic.list import ListView
-from .models import Schedule, Delayed
+from .models import Schedule
 from django.views.generic import CreateView
-from .forms import ScheduleForm, DelayedForm
+from .forms import ScheduleForm,DelayedForm,RescheduleForm
 from . import views
 
 # Create your views here.
@@ -32,11 +32,47 @@ def edit_shipment(request, id):
     if form.is_valid():
         form.save()
 
+        return redirect ('shipments:shipment')
+    context["form"] = form
+    return render(request, "edit_shipment.html", context)
+ 
+
+# def delayedShipmentsList(request):
+#     delayedlist =Schedule.objects.all()
+#     return render (request, 'delayed_shipments.html',{'delayedlist':delayedlist})
+    
+
+def checkDelayedShipments(request):
+    shipments_list =Schedule.objects.filter(status = 'Delayed')
+    return render(request,'delayed_shipments.html',{'shipments_list' : shipments_list })
+
+
+
+
+def countScheduledShipments(request):
+    shipments_count = Schedule.objects.all().count()
+    return render(request,'shipment:shipment',{'shipments_count':shipments_count})
+  
+
+def edit_delay(request, id):
+    context = {}
+
+    reschedule= Schedule.objects.get( id = id)
+    form =RescheduleForm(request.POST or None, instance = reschedule)
+
+
+    if form.is_valid():
+        form.save()
+
         return redirect ('shipments:delayed_shipments')
     context["form"] = form
+    return render(request, "edit_delay.html", context)
 
-    return render(request, "edit_shipment.html", context)
+def checkCompletedShipments(request):
+    completed_list =Schedule.objects.filter(status = 'Completed')
+    return render(request,'completed_shipments.html',{'completed_list' : completed_list })
 
-def delayedShipmentsList(request):
-    delayedlist = Delayed.objects.all()
-    return render (request, 'delayed_shipments.html',{'delayedlist':delayedlist})
+def completed_profile(request, id):
+    view_profile = Schedule.objects.filter(id =id,status = 'Completed')
+
+    return render (request, 'completed_profile.html', {'view_profile': view_profile})
