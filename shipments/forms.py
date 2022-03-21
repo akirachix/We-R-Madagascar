f75b1e2546fabb2b21270e48306832b80976aae0
Wuanitawanjiku <ndichujoyce8@gmail.com>
2022-03-21 11:52:37 +0300

    form changes

--- shipments/forms.py
+++ shipments/forms.py
@@ -2,31 +2,117 @@ from .models import Schedule
 from django.db.models import fields
 from django import forms
 import datetime as dt
-HOUR_CHOICES = [(dt.time(hour=x), '{:02d}:00'.format(x)) for x in range(0, 24)]
-
+from bootstrap_datepicker_plus.widgets import DatePickerInput, TimePickerInput, DateTimePickerInput, MonthPickerInput, YearPickerInput
 
 class ScheduleForm(forms.ModelForm):
 
     class Meta:
         model = Schedule
         fields = ('shipment_id','clinic_name','medication','units','delivery_date','take_of_time','delivery_time','status','destination',)
-        widgets = {'take_of_time': forms.Select(choices=HOUR_CHOICES)}
-        widgets = {'delivery_time': forms.Select(choices=HOUR_CHOICES)}
+        widgets = {
+            'shipment_id': forms.TextInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'clinic_name': forms.TextInput( attrs={'class': 'form-control','style':'width:50%',}),
+            'medication':  forms.TextInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'units': forms.NumberInput(attrs={'class': 'form-control','style':'width:50%'}),  
+            'delivery_date': forms.DateInput( attrs={'class': 'form-control','style':'width:50%'}),
+
+          
+            'take_of_time': forms.TimeInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'delivery_time': forms.TimeInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'status': forms.Select( attrs={'class': 'form-control','style':'width:50%'}),
+
+            'destination': forms.TextInput( attrs={'class': 'form-control','style':'width:50%'}),
+           
+        }
+       
+        labels={
+            'shipment_id':' Shipment Id',
+            'clinic_name': ' Clinic Name',
+            'medication':'Medication',
+            'units':'Units',
+            'take_of_time':'Take of_time',
+            'delivery_time':' Delivery Time',
+            'status':' Status',
+            'destination':' Destination',
+       
+
+
+        }
+
 
 
 class DelayedForm(forms.ModelForm):
 
     class Meta:
         model = Schedule
-        fields = '__all__'
-        widgets = {'take_of_time': forms.Select(choices=HOUR_CHOICES)}
-        widgets = {'delivery_time': forms.Select(choices=HOUR_CHOICES)}
+        fields = ('shipment_id','clinic_name','medication','units','delivery_date','take_of_time','delivery_time','status','destination','delay_reasons')
+        widgets = {
+            'shipment_id': forms.TextInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'clinic_name': forms.TextInput( attrs={'class': 'form-control','style':'width:50%',}),
+            'medication':  forms.TextInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'units': forms.NumberInput(attrs={'class': 'form-control','style':'width:50%'}), 
+            'delivery_date': forms.DateInput( attrs={'class': 'form-control','style':'width:50%'}),
 
+           
+            'take_of_time': forms.TimeInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'delivery_time': forms.TimeInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'status': forms.Select( attrs={'class': 'form-select','style':'width:50%'}),
+
+            'destination': forms.TextInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'delay_reasons': forms.TextInput( attrs={'class': 'form-control','style':'width:50%'}),
+           
+        }
+       
+        labels={
+            'shipment_id':' Shipment Id',
+            'clinic_name': ' Clinic Name',
+            'medication':'Medication',
+            'units':'Units',
+            'take_of_time':'Take of_time',
+            'delivery_time':' Delivery Time',
+            'status':' Status',
+            'destination':' Destination',
+            'delay_reasons': 'Delay reasons',
+       
+
+
+        }
 class RescheduleForm(forms.ModelForm):
 
     class Meta:
         model = Schedule
-        fields = '__all__'
-        widgets = {'take_of_time': forms.Select(choices=HOUR_CHOICES)}
-        widgets = {'delivery_time': forms.Select(choices=HOUR_CHOICES)}
+        fields = ('shipment_id','clinic_name','medication','units','delivery_date','take_of_time','delivery_time','status','destination','delay_reasons')
+        widgets = {
+            'shipment_id': forms.TextInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'clinic_name': forms.TextInput( attrs={'class': 'form-control','style':'width:50%',}),
+            'medication':  forms.TextInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'units': forms.NumberInput(attrs={'class': 'form-control','style':'width:50%'}),
+            'delivery_date': forms.DateInput( attrs={'class': 'form-control','style':'width:50%'}),
+
+            
+            'take_of_time': forms.TimeInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'delivery_time': forms.TimeInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'status': forms.Select( attrs={'class': 'form-select','style':'width:50%'}),
+
+            'destination': forms.TextInput( attrs={'class': 'form-control','style':'width:50%'}),
+            'delay_reasons': forms.TextInput( attrs={'class': 'form-control','style':'width:50%'}),
+
+           
+        }
+       
+        labels={
+            'shipment_id':' Shipment Id',
+            'clinic_name': ' Clinic Name',
+            'medication':'Medication',
+            'units':'Units',
+            'take_of_time':'Take of_time',
+            'delivery_time':' Delivery Time',
+            'status':' Status',
+            'destination':' Destination',
+            'delay_reasons': 'Delay reasons',
+
+       
+
+
+        }
 
