e8e700c7c27f2c6884f48f3550f113dcb0f73206
Shadyaobuya <shadyaobuyagard@gmail.com>
2022-04-07 12:28:26 +0300

    Added Exception handling to csv upload

--- clinic/views.py
+++ clinic/views.py
@@ -1,4 +1,5 @@
 import csv, io
+from django.db import IntegrityError
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from .models import Clinic
@@ -8,43 +9,54 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import permission_required
 
-def clinic_upload(request):
-	template = 'clinic/upload_clinics.html'
-	prompt = {
-		'order': 'order of csv should be name, email, address, profile'
-	}
-	if request.method == "GET":
-		return render(request, template, prompt)
-
-	csv_file = request.FILES["file"]
-
-	if not csv_file.name.endswith('.csv'):
-		messages.error(request,'This is not a csv file')
-
-	data_set = csv_file.read().decode("UTF-8")	
-	io_string = io.StringIO(data_set)
-	next(io_string)
-
-	for column in csv.reader(io_string, delimiter=',', quotechar='|'):
-		_, created = Clinic.objects.update_or_create(
-			name = column[0],
-			email = column[1],
-			address = column[2],
-			profile = column[3]
-		)
-
-	context = {
-	 
-	}
-	return render (request, template, context)
-
 def clinic_display(request):
-	template = 'clinic/view_clinics.html'
-	
-	context = {
-		'clinics': Clinic.objects.all(),
-		'count': Clinic.objects.all().count()
-	}
+    template = 'clinic/view_clinics.html'
+    
+    context = {
+        'clinics': Clinic.objects.all(),
+        'count': Clinic.objects.all().count()
+    }
 
-	return render (request, template, context)
+    return render (request, template, context)
 
+
+def clinic_upload(request):
+   
+    template = "clinic/upload_clinics.html"
+    clinic_data = Clinic.objects.all()
+    try:
+# prompt is a context variable that can have different values      depending on their context
+        prompt = {
+            'order': 'Order of the CSV should be name,email,address, profile',
+            'profiles': clinic_data
+                }
+        # GET request returns the value of the data with the specified key.
+        if request.method == "GET":
+            return render(request, template, prompt)
+        csv_file = request.FILES.get('file')
+        # let's check if it is a csv file
+        if not csv_file.name.endswith('.csv'):
+            messages.error(request, 'THIS IS NOT A CSV FILE')
+        clinic_data_set = csv_file.read().decode('UTF-8')
+        # setup a stream which is when we loop through each line we are able to handle a data in a stream
+        io_string = io.StringIO(clinic_data_set)
+        next(io_string)
+        
+
+        clinic_csvf = csv.reader(io_string, delimiter=',', quotechar="|")
+        clinic_data = []
+        for name,email,address,profile, *__ in clinic_csvf:
+            Clinic.objects.update_or_create(
+                name=name,
+                email=email,
+                address=address,
+                profile=profile
+            )
+        
+
+    except Exception as e: 
+        print(e)
+        return render(request,"clinic/error.html",{'e':e})
+
+    context = {}
+    return render(request, template, context)
