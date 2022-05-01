c7a9ea5358ae15965dc478b0df7676ad4605ec79
Shadyaobuya <shadyaobuyagard@gmail.com>
2022-05-01 21:55:58 +0300

    fixed conflicts

--- flights/urls.py
+++ flights/urls.py
@@ -7,7 +7,4 @@ urlpatterns=[
     path('pending-flights/',PendingRequestsView.as_view(), name='pending_requests'),
     path('scheduled-flights/',ScheduleRequestsView.as_view(), name='scheduled_requests'),
     path('delayed-flights/', DelayedFlightsView.as_view(), name='delayed_requests'),
-    
-    
-   
 ]
