2adaecc1305dd39fbd460e7cf51eb89ac8bb93d6
Shadyaobuya <shadyaobuyagard@gmail.com>
2022-05-01 18:03:39 +0300

    fixed conflicts

--- clinic/urls.py
+++ clinic/urls.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import *
+
+from clinic.views import clinic_upload,clinic_display,search_clinic
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('upload-csv/', clinic_upload, name='clinic_upload'),
-    path('view-clinics/', ClinicViewDetails.as_view(), name='view_clinics'),
+    path('view-clinics/', clinic_display, name='view_clinics'),
     path('search-clinic/', search_clinic, name='search_clinic'),
 ]
-    
